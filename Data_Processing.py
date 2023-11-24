346d980360499a2e8d1bb5f940c99b70270d4e93
krishnamohanpanguluru <krishnamohan.datascience@gmail.com>
2023-11-24 08:50:33 +0530

    Changes performed on the data processing file

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,3 +1,11 @@
 
 
 print("Adding the data processing file in the Github repository")
+print("Add another statemnt of data processing into the central or remote github repository ")
+
+a = 10
+b = 15
+def sum(a,b):
+    c = a+b
+    return a
+print("C")
