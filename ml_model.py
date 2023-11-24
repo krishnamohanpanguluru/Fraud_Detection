bbc070e7b3536cac7b52b1e79b0130773ddef7b2
krishnamohanpanguluru <krishnamohan.datascience@gmail.com>
2023-11-24 15:58:50 +0530

    Adding all ml models

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,15 @@
 
 
 print("all model related code is entered here for git repo")
+
+
+
+def mlmodel1():
+    print(ml_model1)
+
+def mlmodel2():
+    print(mlmodel2)
+
+def mlmodel3():
+    print(mlmodel3)
+    
